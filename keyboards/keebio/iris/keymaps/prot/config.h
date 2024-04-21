84fbb550f8bf533b5ba8ee1839b1f5db71714a82
Protesilaos Stavrou <info@protesilaos.com>
2024-04-21 14:57:02 +0300

    Add initial version of my keyboard setup

---
+++ keyboards/keebio/iris/keymaps/prot/config.h
@@ -0,0 +1,5 @@
+// Copyright 2024 Protesilaos Stavrou <info@protesilaos.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 1000 /* Time (in ms) before the one shot key is released */
